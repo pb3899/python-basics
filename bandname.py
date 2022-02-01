24182af9e7cc2f42f75fe58e3505ecd8aadde7d8
pb3899 <92201539+pb3899@users.noreply.github.com>
2022-02-01 15:32:20 +0530

    Create bandname.py

---
+++ bandname.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("hello there")
+#2. Ask the user for the city that they grew up in.
+city = input("your city name ")
+#3. Ask the user for the name of a pet.
+pet = input ("your pet name ")
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name is "+city+pet)
